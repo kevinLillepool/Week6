82539695cb845dff6f06a0a96fa655de85547995
Kevin Lillepool <kevin.lillepool@tktk.ee>
2024-10-28 21:09:32 +0200

    Week 6

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿string userName = "Darth Vader";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}");
+
+
+//replace
+//a - 4
+
+string updatedUserName = userName.Replace('a', '4').Replace('e', '3');
+
+Console.WriteLine(updatedUserName.Length);  
